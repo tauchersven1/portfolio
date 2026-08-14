13c4344602dd996299d6bd7b7d69bc80fd3bdef5
tauchersven1 <48149051+tauchersven1@users.noreply.github.com>
2026-08-14 06:09:43 +0200

    Add regression tests for option underlying fallback

--- name.abuchen.portfolio.tests/src/name/abuchen/portfolio/snapshot/DerivativePositionCalculatorTest.java
+++ name.abuchen.portfolio.tests/src/name/abuchen/portfolio/snapshot/DerivativePositionCalculatorTest.java
@@ -2,6 +2,7 @@ package name.abuchen.portfolio.snapshot;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertNull;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -10,6 +11,7 @@ import java.util.List;
 import org.junit.Test;
 
 import name.abuchen.portfolio.junit.TestCurrencyConverter;
+import name.abuchen.portfolio.model.Client;
 import name.abuchen.portfolio.model.PortfolioTransaction;
 import name.abuchen.portfolio.model.Security;
 import name.abuchen.portfolio.model.SecurityMultiplier;
@@ -56,5 +58,74 @@ public class DerivativePositionCalculatorTest
                         new TestCurrencyConverter(), date);
 
         assertThat(value, is(Money.of("EUR", Values.Amount.factorize(1000.0))));
+    }
+
+    @Test
+    public void testExistingSecurityConvertedToOptionResolvesUnderlyingByStoredNameWithoutUUID()
+    {
+        Client client = new Client();
+        Security underlying = new Security("Apple Inc.", "USD");
+        underlying.setTickerSymbol("AAPL");
+        client.addSecurity(underlying);
+
+        Security option = new Security("Existing asset", "USD");
+        option.setPropertyValue(SecurityProperty.Type.DERIVATIVE, "type", "OPTION");
+        option.setPropertyValue(SecurityProperty.Type.DERIVATIVE, "underlying", "Apple Inc.");
+        option.setPropertyValue(SecurityProperty.Type.DERIVATIVE, "strike", "200");
+        client.addSecurity(option);
+
+        assertThat(DerivativePositionCalculator.resolveUnderlying(client, option), is(underlying));
+    }
+
+    @Test
+    public void testOptionResolvesUnderlyingByTickerWithoutUUID()
+    {
+        Client client = new Client();
+        Security underlying = new Security("Apple Inc.", "USD");
+        underlying.setTickerSymbol("AAPL");
+        client.addSecurity(underlying);
+
+        Security option = new Security("Option", "USD");
+        option.setPropertyValue(SecurityProperty.Type.DERIVATIVE, "type", "OPTION");
+        option.setPropertyValue(SecurityProperty.Type.DERIVATIVE, "underlying", "aapl");
+        client.addSecurity(option);
+
+        assertThat(DerivativePositionCalculator.resolveUnderlying(client, option), is(underlying));
+    }
+
+    @Test
+    public void testOptionUnderlyingUUIDTakesPrecedenceOverTextFallback()
+    {
+        Client client = new Client();
+        Security byName = new Security("Apple Inc.", "USD");
+        byName.setTickerSymbol("AAPL");
+        Security byUUID = new Security("Different security", "USD");
+        client.addSecurity(byName);
+        client.addSecurity(byUUID);
+
+        Security option = new Security("Option", "USD");
+        option.setPropertyValue(SecurityProperty.Type.DERIVATIVE, "type", "OPTION");
+        option.setPropertyValue(SecurityProperty.Type.DERIVATIVE, "underlying", "Apple Inc.");
+        option.setPropertyValue(SecurityProperty.Type.DERIVATIVE, "underlyingSecurityUUID", byUUID.getUUID());
+        client.addSecurity(option);
+
+        assertThat(DerivativePositionCalculator.resolveUnderlying(client, option), is(byUUID));
+    }
+
+    @Test
+    public void testOptionUnderlyingFallbackRequiresUniqueMatch()
+    {
+        Client client = new Client();
+        Security first = new Security("Index", "EUR");
+        Security second = new Security("Index", "EUR");
+        client.addSecurity(first);
+        client.addSecurity(second);
+
+        Security option = new Security("Option", "EUR");
+        option.setPropertyValue(SecurityProperty.Type.DERIVATIVE, "type", "OPTION");
+        option.setPropertyValue(SecurityProperty.Type.DERIVATIVE, "underlying", "Index");
+        client.addSecurity(option);
+
+        assertNull(DerivativePositionCalculator.resolveUnderlying(client, option));
     }
 }
