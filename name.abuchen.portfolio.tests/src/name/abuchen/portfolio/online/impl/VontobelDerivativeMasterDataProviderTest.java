c2d4ec171030846e4602dd8bae4ce3c2706ad6a6
tauchersven1 <48149051+tauchersven1@users.noreply.github.com>
2026-08-18 16:48:29 +0200

    Use project Hamcrest import in Vontobel tests

--- name.abuchen.portfolio.tests/src/name/abuchen/portfolio/online/impl/VontobelDerivativeMasterDataProviderTest.java
+++ name.abuchen.portfolio.tests/src/name/abuchen/portfolio/online/impl/VontobelDerivativeMasterDataProviderTest.java
@@ -1,7 +1,7 @@
 package name.abuchen.portfolio.online.impl;
 
-import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 import org.junit.Test;
 
