333d8aa9207c9f147724adfecafcd08f46d1e012
tauchersven1 <48149051+tauchersven1@users.noreply.github.com>
2026-09-06 13:14:30 +0200

    Inject active client through supported e4 DI

--- name.abuchen.portfolio.derivatives.addon/src/name/abuchen/portfolio/derivatives/addon/OpenExposureHandler.java
+++ name.abuchen.portfolio.derivatives.addon/src/name/abuchen/portfolio/derivatives/addon/OpenExposureHandler.java
@@ -3,6 +3,7 @@ package name.abuchen.portfolio.derivatives.addon;
 import jakarta.inject.Named;
 
 import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.core.di.annotations.Optional;
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
 import org.eclipse.e4.ui.services.IServiceConstants;
 import org.eclipse.e4.ui.workbench.modeling.EPartService;
@@ -15,17 +16,9 @@ public class OpenExposureHandler
 {
     @Execute
     public void execute(EPartService partService, ExchangeRateProviderFactory factory,
-                    @Named(IServiceConstants.ACTIVE_SHELL) Shell shell)
+                    @Named(IServiceConstants.ACTIVE_SHELL) Shell shell, @Optional Client client)
     {
         MPart activePart = partService.getActivePart();
-
-        if (activePart == null || activePart.getContext() == null)
-        {
-            new ExposureDialog(shell, null, null, null).open();
-            return;
-        }
-
-        Client client = activePart.getContext().get(Client.class);
         new ExposureDialog(shell, client, factory, activePart).open();
     }
 }
