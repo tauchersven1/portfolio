12206cac9d0f19226916d44a41610f9374f61782
tauchersven1 <48149051+tauchersven1@users.noreply.github.com>
2026-09-09 08:11:09 +0200

    Register two exposure reports under Reports

--- derivatives-addon-standalone/de.venari.portfolio.derivatives/src/de/venari/portfolio/derivatives/DerivativesNavigationExtension.java
+++ derivatives-addon-standalone/de.venari.portfolio.derivatives/src/de/venari/portfolio/derivatives/DerivativesNavigationExtension.java
@@ -10,7 +10,9 @@ public class DerivativesNavigationExtension implements NavigationExtension
     {
         registry.addSection("derivatives", "Derivate"); //$NON-NLS-1$ //$NON-NLS-2$
         registry.addView("derivatives", "exposure-management", "Exposure Management", ExposureManagementView.class); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-        registry.addView(NavigationExtensionRegistry.SECTION_REPORTS, "exposure-report", "Exposure", //$NON-NLS-1$ //$NON-NLS-2$
-                        ExposureReportView.class);
+        registry.addView(NavigationExtensionRegistry.SECTION_REPORTS, "long-short-exposure-report", //$NON-NLS-1$
+                        "Bestand - Long / Short Exposure", LongShortExposureReportView.class); //$NON-NLS-1$
+        registry.addView(NavigationExtensionRegistry.SECTION_REPORTS, "total-exposure-report", //$NON-NLS-1$
+                        "Bestand - Gesamtexposure", ExposureReportView.class); //$NON-NLS-1$
     }
 }
