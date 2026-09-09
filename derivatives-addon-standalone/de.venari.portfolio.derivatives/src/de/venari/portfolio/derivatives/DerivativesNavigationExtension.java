eb38ceafd48f3ac27dbb37ea6216947d5a52d2fc
tauchersven1 <48149051+tauchersven1@users.noreply.github.com>
2026-09-09 13:48:25 +0200

    Add dated derivative security master data

--- derivatives-addon-standalone/de.venari.portfolio.derivatives/src/de/venari/portfolio/derivatives/DerivativesNavigationExtension.java
+++ derivatives-addon-standalone/de.venari.portfolio.derivatives/src/de/venari/portfolio/derivatives/DerivativesNavigationExtension.java
@@ -8,8 +8,6 @@ public class DerivativesNavigationExtension implements NavigationExtension
     @Override
     public void contribute(NavigationExtensionRegistry registry)
     {
-        registry.addSection("derivatives", "Derivate"); //$NON-NLS-1$ //$NON-NLS-2$
-        registry.addView("derivatives", "exposure-management", "Exposure Management", ExposureManagementView.class); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
         registry.addView(NavigationExtensionRegistry.SECTION_REPORTS, "long-short-exposure-report", //$NON-NLS-1$
                         "Bestand - Long / Short Exposure", LongShortExposureReportView.class); //$NON-NLS-1$
         registry.addView(NavigationExtensionRegistry.SECTION_REPORTS, "total-exposure-report", //$NON-NLS-1$
